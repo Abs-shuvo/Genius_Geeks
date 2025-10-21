96d7d98459c6a35e7039ea7d28041e16cea76175
Abu Bakor Siddik (Shuvo) <141806136+Abs-shuvo@users.noreply.github.com>
2025-10-21 14:55:10 +0600

    Create database.php

---
+++ config/database.php
@@ -0,0 +1,26 @@
+<?php
+// config/database.php
+class Database {
+    private $host = "localhost";
+    private $db_name = "genius_geeks";
+    private $username = "root";
+    private $password = "";
+    public $conn;
+
+    public function getConnection() {
+        $this->conn = null;
+        try {
+            $this->conn = new PDO(
+                "mysql:host={$this->host};dbname={$this->db_name}",
+                $this->username,
+                $this->password
+            );
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $this->conn->exec("set names utf8mb4");
+        } catch(PDOException $e) {
+            die("Connection failed: " . $e->getMessage());
+        }
+        return $this->conn;
+    }
+}
+?>
